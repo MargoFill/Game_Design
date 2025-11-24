135f314492812ce2666322db960af55c391aa49c
meryem0798 <161157254+meryem0798@users.noreply.github.com>
2025-11-24 16:31:31 +0900

    Delete do-gs-project directory in meryem branch

--- do-gs-project/scripts/serve.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-@export var coffee_machine: StaticBody3D
-
-func _on_serve_button_pressed() -> void:
-	%CoffeeMachine3D._finish_coffee()
--- do-gs-project/scripts/trash.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-
-func _on_button_pressed() -> void:
-	%coffee_cup._delete()
-	%cup_sprite._reset()
