ffc91493696ef68e4c9b23e498d6472b4ad20b61
meryem0798 <meryemdouibi07980@gmail.com>
2025-12-01 11:19:22 +0900

    Added hover 'serve' button for the mini game

--- do-gs-project/scenes/coffee_minigame.gd
+++ do-gs-project/scenes/coffee_minigame.gd
@@ -1,8 +1,14 @@
 extends Node2D
 @onready var serve_button: Button = $serve/Sprite2D/serve_button
+@onready var serve_hover: Sprite2D = $serve_hover
 
 const _3D_SCENE = preload("res://scenes/3d_scene.tscn")
+@onready var sprite_2d: Sprite2D = $serve/Sprite2D
 
+func _ready() -> void:
+	serve_hover.visible = false
+	
+	
 var player: Node = null
 func set_player(p):
 	player = p
@@ -12,3 +18,13 @@ func _on_serve_button_pressed() -> void:
 	print("Milkpumps: ", Global_Values.milkpumps, " Added Coffee: ", Global_Values.added_coffee, " Added Strawberry: ", Global_Values.added_strawberry, " Added Chocholate: ", Global_Values.added_chocolate, " Added Vanilla: ", Global_Values.added_vanilla)
 	queue_free()
 	get_tree().current_scene.process_mode = Node.PROCESS_MODE_INHERIT
+
+
+
+
+func _on_serve_button_mouse_entered() -> void:
+	serve_hover.visible = true
+	
+
+func _on_serve_button_mouse_exited() -> void:
+	serve_hover.visible = false
--- do-gs-project/scripts/menu.gd
+++ do-gs-project/scripts/menu.gd
@@ -1,13 +1,51 @@
 extends Control
+@onready var hover_start: TextureRect = $hover_start
+@onready var hover_settings: TextureRect = $hover_settings
+@onready var hover_exit: TextureRect = $hover_exit
 
 
-func _on_start_button_pressed() -> void:
+
+func _ready() -> void:
+	hover_start.visible = false
+	hover_settings.visible = false
+	hover_exit.visible = false
+	
+func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file('res://scenes/3d_scene.tscn')
 
 
-func _on_options_button_pressed() -> void:
+func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_file('res://scenes/options.tscn')
 
 
-func _on_exit_button_pressed() -> void:
+func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+
+
+func _on_start_mouse_entered() -> void:
+	hover_start.visible = true
+
+
+func _on_start_mouse_exited() -> void:
+	hover_start.visible = false
+	
+	
+	
+
+
+func _on_settings_mouse_entered() -> void:
+	hover_settings.visible = true
+
+
+func _on_settings_mouse_exited() -> void:
+	hover_settings.visible = false
+
+
+func _on_exit_mouse_entered() -> void:
+	hover_exit.visible = true
+
+
+func _on_exit_mouse_exited() -> void:
+	hover_exit.visible = false
